cd5bba45fa48426cc063f1267a98eb592aa7dc4f
Mostafa <mostafafawzy7107@gmail.com>
2024-02-13 02:26:10 +0200

    SCKT-1 | Types - Remove unnecessary types

--- src/types.ts
+++ src/types.ts
@@ -1,25 +1,3 @@
-export enum HttpMethod {
-  GET = "GET",
-  POST = "POST",
-  PUT = "PUT",
-  DELETE = "DELETE",
-  PATCH = "PATCH",
-}
-  
-export interface ApiResponse<T> {
-  status: number;
-  data?: T;
-  errors?: string[];
-}
-
-
-export interface UnauthorizedResponse {
-  status: 401;
-  error: string;
-}
-
-export type ApiClientResponse<T> = ApiResponse<T> | UnauthorizedResponse;
-  
 export interface PaginationLink {
   url: string | null;
   label: string;
